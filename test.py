43575232cab47a011e2e3d8e836b3d1426833d8e
leeeha <jxlhe46@seoultech.ac.kr>
2022-01-15 00:31:16 +0900

    Joining Images : 스택에 이미지를 쌓아서 이어붙이기

---
+++ test.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+# 이미지를 스택에 쌓는 함수
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+img = cv2.imread("Resources/lena.jpg")
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgStack = stackImages(0.5, ([img, imgGray, img],[img, img, img])) # 2행 3열
+
+# imgHor = np.hstack((img,img)) # 두 이미지의 채널 번호는 동일해야 한다.
+# imgVer = np.vstack((img,img))
+#
+# cv2.imshow("Horizontal",imgHor)
+# cv2.imshow("Vertical",imgVer)
+
+cv2.imshow("ImageStack",imgStack)
+
+cv2.waitKey(0)
