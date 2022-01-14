edde8ade541ee4ec3a5960d67e8295a3f71c15c2
leeeha <jxlhe46@seoultech.ac.kr>
2022-01-14 23:52:36 +0900

    Warp perspective : 찌그러진 사각형을 직각으로 편다!

--- chapter1.py
+++ chapter1.py
@@ -1,19 +1,15 @@
 import cv2
 import numpy as np
 
-# 이미지는 단지 픽셀로 이루어진 배열, 행렬이다.
+img = cv2.imread("Resources/pill.jpg")
 
-img = cv2.imread("Resources/lena.jpg")
-print(img.shape) # 높이, 너비, BGR 채널 번호
-
-imgResize = cv2.resize(img, (200, 200))
-print(imgResize.shape)
-
-imgCropped = img[0:100, 100:200] # 높이, 너비 [시작점: 끝점]
-print(imgCropped.shape)
+width, height = 400, 250
+pts1 = np.float32([[1137, 1193], [2553, 2113], [377, 1777], [1793, 2993]])
+pts2 = np.float32([[0,0], [width,0], [0, height], [width, height]])
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
 
 cv2.imshow("Image", img)
-cv2.imshow("Image Resize", imgResize)
-cv2.imshow("Image Crop", imgCropped)
+cv2.imshow("Output", imgOutput)
 
 cv2.waitKey(0)
